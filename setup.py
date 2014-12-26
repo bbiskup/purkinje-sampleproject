26a8a214d016129489bf347afe97422c5726df87
Bernhard Biskup <bbiskup@gmx.de>
2014-12-26 21:24:28 +0100

    Fixed setup/tox.ini; added MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+readme = open('README.rst').read()
+
+
+def parse_requirements():
+    with open('requirements.txt') as req:
+        return [x for x in req.readlines() if not x.startswith('-e ')]
+
+
+setup(name='flotsam',
+      version='0.1.0',
+      description='Sample project for purkinje test runner',
+      author='Bernhard Biskup',
+      author_email='bbiskup@gmx.de',
+      url='biskup-software.de',
+      package_dir={'purkinje_sampleproject': 'purkinje_sampleproject'},
+      packages=['purkinje_sampleproject'],
+      zip_safe=False,
+      include_package_data=True,
+      install_requires=parse_requirements(),
+      classifiers={
+          'Development Status :: 2 - Pre-Alpha',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          # Topic :: Software Development :: Libraries :: Python Modules',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+      },
+      license='The MIT License (MIT)',
+      keywords='flotsam utilities',
+      tests_require=['tox'],
+      )
