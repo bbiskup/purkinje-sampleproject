268bf78c09af92e1a4199cbe744f889c685d5d1c
Bernhard Biskup <bbiskup@gmx.de>
2015-01-04 17:21:40 +0100

    New multi test case

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -1,6 +1,21 @@
+import pytest
+import random
+
+rand_sep = random.randint(0, 10)
+
+
 def test_1():
     assert True
 
 
 def test_2():
-    assert True
+    assert False
+
+
+# @pytest.mark.parametrize('arg', range(5000))
+# def test_3(arg):
+#     assert arg % 2 == 0
+
+@pytest.mark.parametrize('arg', range(500))
+def test_3(arg):
+    assert random.randint(0, 10) > rand_sep
