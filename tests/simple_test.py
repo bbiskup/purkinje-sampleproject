5947597c441b3a85e6d9cbf49488e5e0789868eb
Bernhard Biskup <bbiskup@gmx.de>
2015-01-07 19:47:18 +0100

    New sample test cases

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -13,11 +13,29 @@ def test_2():
     assert False
 
 
+@pytest.mark.skipif('True')
+def test_3():
+    assert False
+
+
 # @pytest.mark.parametrize('arg', range(5000))
 # def test_3(arg):
 #     assert arg % 2 == 0
 
-@pytest.mark.parametrize('arg', range(200))
-def test_3(arg):
-    time.sleep(0)
+# @pytest.mark.parametrize('arg', range(3))
+# def test_3(arg):
+#     time.sleep(0)
+#     assert random.randint(0, 10) > rand_sep
+
+
+# @pytest.mark.slow
+# def test_4():
+#     time.sleep(5)
+#     assert True
+
+
+@pytest.mark.slow
+@pytest.mark.parametrize('arg', range(30))
+def test_5(arg):
+    time.sleep(1)
     assert random.randint(0, 10) > rand_sep
